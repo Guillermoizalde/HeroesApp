0a710db19c303578fe91afe578f7ec4daf771161
Guillermo Izalde <izalde.guillermo45@gmail.com>
2022-03-22 06:26:02 -0600

    Lista de Heroes

---
+++ src/selectors/getHeroById.js
@@ -0,0 +1,9 @@
+import { heroes } from "../data/heroes"
+
+
+
+export const getHeroById = ( id = '') => {
+    console.log('getHeroById called');
+
+    return heroes.find( hero => hero.id === id);
+}

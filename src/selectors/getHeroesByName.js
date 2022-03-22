091f08769bd8089f688b5b88c9ec5870b48cc756
Guillermo Izalde <izalde.guillermo45@gmail.com>
2022-03-22 07:30:11 -0600

    Filtro de Heroes

--- src/selectors/getHeroesByName.js
+++ src/selectors/getHeroesByName.js
@@ -9,3 +9,18 @@ export const getHeroesByName = (name = '') => {
     return [...heroes];
 
 }
+
+export const getHeroesByName = (name = '') => {
+
+    //name = name.toLowerCase();
+    if ( name.length === 0 ) {
+        return [];
+    }
+
+    //return heroes.filter(hero => hero.name.toLowerCase().includes(name));
+
+    return [...heroes];
+    name = name.toLowerCase();
+    return heroes.filter(hero => hero.superhero.toLowerCase().includes(name));
+
+}
